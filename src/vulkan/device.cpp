c864090d673ce984b3b88fb5ab89a430e8f43b21
Alexis Dutaud <dutaud.alex@gmail.com>
2026-08-18 23:01:58 +0200

    fix requiredFeatures.shaderDemoteToHelperInvocation wasn't properly validate, added to physical device and then to logical device

--- src/vulkan/device.cpp
+++ src/vulkan/device.cpp
@@ -41,6 +41,7 @@ Device::Device(const PhysicalDevice& physicalDevice) {
     features13.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES;
     features13.dynamicRendering = VK_TRUE;
     features13.synchronization2 = VK_TRUE;
+    features13.shaderDemoteToHelperInvocation = VK_TRUE;
 
     VkPhysicalDeviceFeatures2 features{};
     features.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_FEATURES_2;
